0498e7c153cbd58514af79facac9b9332ef54feb
danynoire <backupsdwnskj.co@gmail.com>
2026-01-19 17:07:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,19 @@
 import discord
 from discord.ext import commands
 from config import TOKEN, PREFIX
-from player import get_player
-from dashboard import MusicDashboard, music_embed
-from checks import owner_only
+from help import setup_help
 
 intents = discord.Intents.all()
 
-bot = commands.Bot(
-    command_prefix=PREFIX,
-    intents=intents
-)
+bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 @bot.event
 async def on_ready():
+    for ext in ["cogs.music", "cogs.info", "cogs.admin"]:
+        await bot.load_extension(ext)
+
+    setup_help(bot)
     await bot.tree.sync()
     print("🤖 Bot online")
-
-@commands.hybrid_command()
-async def join(ctx):
-    if ctx.author.voice:
-        await ctx.author.voice.channel.connect()
-        await ctx.send("🎧 Conectado")
-
-@commands.hybrid_command()
-async def play(ctx, *, query):
-    player = get_player(ctx.guild)
-
-    if not ctx.guild.voice_client:
-        await ctx.author.voice.channel.connect()
-
-    await player.add(query)
-
-    await ctx.send(
-        embed=music_embed(player),
-        view=MusicDashboard(player)
-    )
-
-@commands.hybrid_command()
-async def skip(ctx):
-    ctx.guild.voice_client.stop()
-    await ctx.send("⏭ Pulou")
-
-@commands.hybrid_command()
-async def stop(ctx):
-    await ctx.guild.voice_client.disconnect()
-    await ctx.send("⏹ Parou")
-
-@commands.hybrid_command()
-@owner_only()
-async def shutdown(ctx):
-    await ctx.send("🔌 Desligando bot")
-    await bot.close()
 
 bot.run(TOKEN)
