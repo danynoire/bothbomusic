5817ca1f086158b45b5d881109d5605e0889881a
danynoire <backupsdwnskj.co@gmail.com>
2026-01-19 18:19:49 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, request, abort, render_template
+import os
 
-def run_dashboard(bot):
+def run_dashboard():
     app = Flask(__name__)
+    app.secret_key = os.getenv("FLASK_SECRET")
+
+    DASH_TOKEN = os.getenv("DASHBOARD_TOKEN")
+
+    def check():
+        token = request.args.get("token")
+        if token != DASH_TOKEN:
+            abort(403)
 
     @app.route("/")
     def home():
-        return "HB Music Dashboard Online ✅"
+        check()
+        return render_template("admin.html")
 
-    print("🌐 Dashboard online")
+    print("🌐 Dashboard online (token)")
     app.run(host="0.0.0.0", port=10000)
